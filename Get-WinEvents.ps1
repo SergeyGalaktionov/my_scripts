07b31243bd4aac0a8a8c7df05717b032e64d915c
Sergey S. Galaktionov <sergey.galaktionov@live.ru>
2025-09-13 20:51:06 +0300

    Event script

---
+++ Get-WinEvents.ps1
@@ -0,0 +1,12 @@
+﻿$time = (Get-Date) - (New-TimeSpan -Days 365)
+$result = Get-WinEvent -FilterHashtable @{logname='Security';id=4732,4728;StartTime=$Time} -ErrorAction SilentlyContinue| ForEach-Object {
+    $eventXml = ([xml]$_.ToXml()).Event
+    [PSCustomObject]@{
+        TimeCreated   = $eventXml.System.TimeCreated.SystemTime -replace '\.\d+.*$'
+        NewUser = $eventXml.EventData.Data[0]."#text"
+        Group = $eventXml.EventData.Data[2]."#text"
+        Admin = $eventXml.EventData.Data[6]."#text"
+        Computer      = $eventXml.System.Computer
+    }
+}
+$result | Format-Table -AutoSize 
