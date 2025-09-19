e804e3aadebba001df24d8d7c8ffcd31067cab4b
Sergey S. Galaktionov <sergey.galaktionov@live.ru>
2025-09-19 10:23:15 +0300

    some shit

---
+++ New-VPNConnection.ps1
@@ -0,0 +1,25 @@
+﻿$VpnName = "SSTP SELECTEL"  
+$ServerAddress = "sstp.galaktionov.tech"  
+$DnsSuffix = "dt.selectel"
+
+# Создать новое SSTP VPN подключение
+$VpnParams = @{
+Name = $VpnName
+ServerAddress = $ServerAddress
+TunnelType = 'Sstp'
+AuthenticationMethod = 'MSChapv2'
+SplitTunneling = $true
+RememberCredential = $true
+UseWinlogonCredential = $false
+DnsSuffix = $DnsSuffix
+IdleDisconnectSeconds = 3600
+EncryptionLevel = 'Maximum'
+}
+
+Add-VpnConnection @VpnParams
+
+# Добавить маршрут
+Add-VpnConnectionRoute -ConnectionName $VpnName -DestinationPrefix "192.168.1.0/24"
+   
+# Добавить NRPT правило
+Add-DnsClientNrptRule -Namespace "dt.selectel" -NameServers "192.168.1.3","192.168.1.2"
